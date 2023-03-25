803a5a55b5bdb5bf5de465e5188fd09155496b93
mahrus-rohisyam <mahrusaim@gmail.com>
2023-03-25 13:32:39 +0700

    fix: config middleware.js, modify: .env.example and disable strapi telemetry

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,23 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  {
+    name: "strapi::poweredBy",
+    config: {
+      poweredBy: "Salt Academy <life.saltacademy.id",
+    },
+  },
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  {
+    name: "strapi::session",
+    config: {
+      rolling: true,
+      renew: true,
+    },
+  },
+  "strapi::favicon",
+  "strapi::public",
 ];
